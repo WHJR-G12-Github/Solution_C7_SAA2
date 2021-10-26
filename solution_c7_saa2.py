aeb6e54493c2e8bf23383a14081cc00222e625c1
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-26 14:32:06 +0530

    Create solution_c7_saa2.py

---
+++ solution_c7_saa2.py
@@ -0,0 +1,11 @@
+# Creating a list
+num = [2,4,6,8,10]
+
+# Printing the list before insertion
+print("Before Insertion:",num)
+
+# Inserting an element '20' at position '3'
+num.insert(3,20)
+
+# Printing the list after insertion
+print("After Insertion:",num)
